328875ac814c9ad71f909ea973ce106173698921
VitalyHmelnov <vit.khmelnov@yandex.ru>
2022-10-09 01:27:42 +0300

    first commit

---
+++ Game_PilotBrothersSafe/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Game_PilotBrothersSafe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите размер";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(303, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(97, 40);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LawnGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(859, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Game_PilotBrothersSafe.Resource1.safe_loced;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1678, 1050);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сейф Братьев Пилотов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Game_PilotBrothersSafe/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_PilotBrothersSafe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+        
+        PictureBox[,] pb;
+        bool [,] Arr = new bool[10, 10];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int n = int.Parse(comboBox1.Text);            
+            Controls.Clear();
+            pb = new PictureBox[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Arr[i, j] = true;
+                    pb[i, j] = new PictureBox();
+                    pb[i, j].Location = new System.Drawing.Point(330 + i * 50, 100 + j * 50);
+                    pb[i, j].Size = new System.Drawing.Size(50, 50);
+                    pb[i, j].TabIndex = i;
+                    pb[i, j].BackColor = Color.Transparent;
+                    pb[i, j].Image = Resource1.handle;
+                    Controls.Add(pb[i, j]);
+                    int row = i;
+                    int column = j;
+                    pb[i, j].Click += (x, y) => { pbClick(row, column); };
+                }
+            }
+            pb[0, 0].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            Arr[0, 0] = false;
+            for (int i = 1; i < n; i++)
+            {
+                Arr[0, i] = false;
+                Arr[i, 0] = false;
+                pb[0, i].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pb[i, 0].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+
+        }
+
+        void pbClick(int row, int column)
+        {
+            int n = int.Parse(comboBox1.Text);
+            int count = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (i == row || j == column)
+                    {
+                        if (Arr[i, j] == true)
+                        {
+                            Arr[i, j] = false;
+                            Image image = pb[i, j].Image;
+                            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            pb[i, j].Image = image;
+                        }
+                        else
+                        {
+                            Arr[i, j] = true;
+                            Image image = pb[i, j].Image;
+                            image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            pb[i, j].Image = image;
+                        }
+                        
+                    }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (Arr[i,j]==true)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+            }
+            if (count==n*n||count==-n*n)
+            {
+                Controls.Clear();
+                BackgroundImage = Resource1.safe_open;
+                label1 = new Label();
+                label1.Text = "Поздравляю, ты открыл сейф";
+                label1.Location = new System.Drawing.Point(0, 60);
+                label1.Size = new System.Drawing.Size(500, 20);
+                Controls.Add(label1);
+
+            }
+        }
+        
+    }
+}
